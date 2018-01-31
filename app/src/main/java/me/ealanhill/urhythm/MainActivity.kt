bf73ce214b48f4d6676f45cb83025eab7a4c0996
Alan Hill <hill.e.alan@gmail.com>
2018-01-31 15:14:14 -0600

    Create Collapsing Toolbar
     * creates the collapsing toolbar with the overlapping content
     * adds the app colors to theme
     * styles the toolbar

--- app/src/main/java/me/ealanhill/urhythm/MainActivity.kt
+++ app/src/main/java/me/ealanhill/urhythm/MainActivity.kt
@@ -1,5 +1,16 @@
 package me.ealanhill.urhythm
 
-/**
- * Created by alan on 1/31/18.
- */
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+
+}
