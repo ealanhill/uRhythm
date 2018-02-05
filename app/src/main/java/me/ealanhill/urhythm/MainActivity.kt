9c353c53ab07789ea4119b5911152e6a3a0be3db
Alan Hill <hill.e.alan@gmail.com>
2018-02-05 10:00:37 -0600

    Weekly Line Graph
     * adds the MPAndroidChart to show the users energy throughout the day
     * formats the line graph to looke like a nice sparkline

--- app/src/main/java/me/ealanhill/urhythm/MainActivity.kt
+++ app/src/main/java/me/ealanhill/urhythm/MainActivity.kt
@@ -1,8 +1,18 @@
 package me.ealanhill.urhythm
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +21,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+
+        setData()
+
+        weeklyAverageChart.invalidate()
+    }
+
+    private fun setData() {
+        weeklyAverageChart.setBackgroundColor(Color.rgb(255, 255,255))
+        weeklyAverageChart.setDrawGridBackground(false)
+        weeklyAverageChart.description = null
+
+        // set up x axis
+        weeklyAverageChart.xAxis.setDrawGridLines(false)
+        weeklyAverageChart.xAxis.setDrawAxisLine(false)
+        weeklyAverageChart.xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
+
+        // set up y axis
+        weeklyAverageChart.axisLeft.isEnabled = false
+        weeklyAverageChart.axisRight.isEnabled = false
+        weeklyAverageChart.legend.isEnabled = false
+
+        // temporary data
+        val values = (1..7).map { it -> Entry(it.toFloat(), Random().nextInt(300 - 50) + 50f) }
+
+        val dataSet = LineDataSet(values, "Test Data")
+        dataSet.axisDependency = YAxis.AxisDependency.LEFT
+        // smoothes out the line
+        dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        dataSet.color = ContextCompat.getColor(this, R.color.colorPrimary)
+        dataSet.lineWidth = 3f
+        dataSet.setDrawValues(false)
+        dataSet.setDrawCircles(false)
+
+        val lineData = LineData(dataSet)
+
+        weeklyAverageChart.data = lineData
     }
 
 }
